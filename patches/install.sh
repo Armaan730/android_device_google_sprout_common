46adcd6acc6c0c9c467eb775be95acf7d969bd1d
SamarV-121 <samarvispute121@gmail.com>
2019-04-06 12:57:44 +0000

    sprout: Add earlysuspend support
    * Fixes deepsleep
    
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

---
+++ patches/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ROOT="${PWD}"
+REPOSITORIES=(
+    'system/core'
+ )
+
+for repository in "${REPOSITORIES[@]}"; do
+    cd "${ROOT}/${repository}"
+    echo "${RED}Applying ${NC}${repository} ${RED}patches...${NC}"
+    patch -p1 < "${ROOT}/device/google/sprout/patches/${repository}"/*
+    cd "${ROOT}"
+done
