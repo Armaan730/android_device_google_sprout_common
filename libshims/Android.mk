4d5c8ddecc65d52f742a0645621f669da2d7bf15
SamarV-121 <samarvispute121@gmail.com>
2019-04-06 12:51:54 +0000

    sprout: Shim libnetutils
    * Add MTK support func
    
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -3,12 +3,12 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 # /system/lib/librilmtk.so ( _ZN7android6Parcel13writeString16EPKtj )
-LOCAL_SRC_FILES += ril.cpp
-LOCAL_SHARED_LIBRARIES += libbinder
+LOCAL_SRC_FILES := ril.cpp
+LOCAL_SHARED_LIBRARIES := libbinder
 
 # /system/lib/libcam.utils.so ( _ZN7android13GraphicBufferC1Ejjij )
 LOCAL_SRC_FILES += GraphicBuffer.cpp
-LOCAL_SHARED_LIBRARIES := libui
+LOCAL_SHARED_LIBRARIES += libui
 
 # /system/bin/mtk_agpsd ( UCNV_FROM_U_CALLBACK_STOP_55 )
 LOCAL_SRC_FILES += icu55.c
@@ -17,6 +17,11 @@ LOCAL_SHARED_LIBRARIES += libicuuc
 # /system/lib/libmtkjpeg.so ( __pthread_gettid )
 LOCAL_SRC_FILES += bionic.cpp
 LOCAL_SHARED_LIBRARIES += libc
+
+# /system/lib/libnetutils.so ( ifc_set_txq_state )
+LOCAL_SRC_FILES += netutils.c
+LOCAL_SHARED_LIBRARIES += liblog libcutils
+LOCAL_C_INCLUDES += system/core/libnetutils/include
 
 # /system/vendor/lib/libwvm.so ( _ZN7android16MediaBufferGroupC1Ev , _ZNK7android11MediaSource11ReadOptions9getSeekToEPxPNS1_8SeekModeE , _ZNK7android11MediaSource11ReadOptions14getNonBlockingEv , _ZN7android16MediaBufferGroup14acquire_bufferEPPNS_11MediaBufferEb
 # LOCAL_SRC_FILES := wvm.cpp
